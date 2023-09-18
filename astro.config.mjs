8433b7252be70b85f81423d1b0d274851eb55750
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2023-09-18 16:11:20 -0600

    refactor: :rocket: Redirección a blog
    Se redirigio a la pagina de blog ya que este es el unico que esta en funcionamiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [tailwind(),react()],
-});
+  redirects: {
+    '/': '/blog'
+  },
+  integrations: [tailwind(), react()]
+})
