0a6baf0e03880c887dcb79d345de978ee018090f
Francisco Huchin <franciscouchuchin@gmail.com>
2024-08-02 18:55:03 -0600

    Remove redirrection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  redirects: {
-    '/': '/blog'
-  },
+  // redirects: {
+  //   '/': '/blog'
+  // },
   integrations: [tailwind(), react()]
 })
