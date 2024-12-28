4183f422b5f508c580d6d3a66bd82dd5d243d0c3
Francisco Huchin <franciscouchuchin@gmail.com>
2024-12-27 21:07:52 -0600

    Enhance Tailwind CSS configuration with custom header animation and keyframes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 const { withAnimations } = require('animated-tailwindcss') // eslint-disable-line
-const defaultTheme = require('tailwindcss/defaultTheme') // eslint-disable-line
+const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withAnimations({
@@ -7,10 +7,33 @@ module.exports = withAnimations({
   darkMode: 'class', // or 'media'
   theme: {
     extend: {
-      sans: {
-        fontFamily: ['Poppins', ...defaultTheme.fontFamily.sans]
+      keyframes: {
+        'reduce-header': {
+          '100%': {
+            boxShadow: '0 5px 50px -5px #ffffff1a, 0 0 0 1px #ffffff1a',
+            background: '#0000004d',
+            paddingBlock: '1rem',
+            backdropFilter: 'blur(10px)',
+            WebkitBackdropFilter: 'blur(10px)'
+          }
+        }
       }
     }
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [
+    function ({ addComponents, theme }) {
+      addComponents({
+        '.header-animate': {
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          right: '0',
+          animation: 'reduce-header linear both',
+          animationTimeline: 'scroll()',
+          animationRange: '0 150px'
+        }
+      })
+    },
+    require('@tailwindcss/typography')
+  ]
 })
