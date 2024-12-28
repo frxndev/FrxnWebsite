1611dc06b12fa3f7731d889638f97efa89d8f0b1
Francisco Huchin <franciscouchuchin@gmail.com>
2024-12-28 17:05:06 -0600

    feat: enhance base64 validation to check expected MIME type

--- src/lib/convert-base64-blob.ts
+++ src/lib/convert-base64-blob.ts
@@ -5,11 +5,25 @@ function base64ToBlob(base64: string, mimeType: string): Blob {
   return new Blob([byteArray], { type: mimeType })
 }
 
-function isValidBase64(base64: string): boolean {
+function isValidBase64(base64: string, expectedMimeType: string): boolean {
   const base64Regex =
     /^(?:[A-Za-z0-9+/]{4})*?(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/
 
-  return base64Regex.test(base64) && base64.length % 4 === 0
+  if (!base64Regex.test(base64) || base64.length % 4 !== 0) return false
+
+  try {
+    const decodedData = atob(base64)
+    const uint8Array = new Uint8Array(
+      Array.from(decodedData).map((char) => char.charCodeAt(0))
+    )
+
+    const blob = new Blob([uint8Array])
+    const detectedMimeType = blob.type
+
+    return detectedMimeType === expectedMimeType
+  } catch (error) {
+    return false
+  }
 }
 
 export { base64ToBlob, isValidBase64 }
