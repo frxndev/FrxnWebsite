c9a690513658b3adfb20bfdc70eb7ef4f7928ac1
Francisco Huchin <franciscouchuchin@gmail.com>
2025-03-02 23:06:32 -0600

    Migrate new version of tailwind css and migrate custom theme construction

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
 export default {
-	plugins: {
-		'postcss-import': {},
-		'tailwindcss/nesting': 'postcss-nesting',
-		tailwindcss: {},
-		autoprefixer: {},
-		...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-	},
-};
+  plugins: {
+    '@tailwindcss/postcss': {}
+  }
+}
